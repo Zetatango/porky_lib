ecfa77f7953c6d17229ec65c9d43c0ae90820a1e
Weiyun Lu <wlu058@uottawa.ca>
2020-01-06 14:59:57 -0500

    Alphabetize imports

--- lib/porky_lib.rb
+++ lib/porky_lib.rb
@@ -10,12 +10,10 @@ module PorkyLib
   require 'porky_lib/file_service'
   require 'porky_lib/models/application_record'
   require 'porky_lib/models/encryption_key'
+  require 'porky_lib/railtie' if defined?(Rails) # for the rake tasks
   require 'porky_lib/services/key_management_service'
   require 'porky_lib/symmetric'
   require 'porky_lib/validators/string_validator'
   require 'porky_lib/version'
   require 'porky_lib/unencrypted'
-  require 'porky_lib/caching_encryptor'
-  require 'porky_lib/concerns/has_encrypted_attributes'
-  require "porky_lib/railtie" if defined?(Rails) # for the rake tasks
 end
